3e7c6321522264847a90ace7d3e49d17db39fc34
vinceplayz91-lab <vinceplayz91@gmail.com>
2026-03-04 22:14:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const heartsContainer = document.querySelector('.hearts-container');
 function createHeart() {
   const heart = document.createElement('div');
   heart.classList.add('heart');
-  heart.style.left = Math.random() * 100 + 'vw';
+  heart.style.left = Math.random() * 19 + 'vw';
   heart.style.animationDuration = (3 + Math.random() * 2) + 's';
   heartsContainer.appendChild(heart);
   setTimeout(() => heart.remove(), 5000);
@@ -88,5 +88,6 @@ function gameLoop() {
 
   requestAnimationFrame(gameLoop);
 }
+
 
 gameLoop();
